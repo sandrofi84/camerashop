ffe764b90e627b49b17a895465b7780914272f26
Sandro <sandrofillinich@gmail.com>
2021-02-27 09:33:11 +0000

    Added integration to appsync

--- src/components/shopPool.js
+++ src/components/shopPool.js
@@ -1,18 +1,67 @@
 import React, { useEffect, useState } from 'react'
 import ProductCard from '../components/productCard'
 import Axios from 'axios'
-import io from 'socket.io-client'
+import { gql, ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client'
+import { createAuthLink } from 'aws-appsync-auth-link';
+import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
+import config from '../config/appsyncConfig'
+
+
 
 const ShopPool = ({products, site}) => {
     const [stock, setStock] = useState();
     
 
     useEffect(() => {
-        const socket = io("https://api.sandrofi.com:60000", {path: '/camerashop/socket.io'});
-        socket.on("inventory changed", () => {
-            console.log("Getting New Inventory");
-            getStock()
-         })
+
+        const region = config.APPSYNC_REGION;
+        const appsyncId = config.APPSYNC_GRAPHQL_API_ID
+        const url2 = 'ws://192.168.1.221:20002'
+        const url = `wss://${appsyncId}.appsync-api.${region}.amazonaws.com/graphql`;
+        const auth = {
+        type: config.APPSYNC_AUTH_TYPE,
+        apiKey: config.APPSYNC_API_KEY,
+        };
+
+        const link = ApolloLink.from([
+        createAuthLink({ url2, region, auth }),
+        createSubscriptionHandshakeLink({url2, region, auth})
+        ]);
+        
+        const client = new ApolloClient({
+            link,
+            cache: new InMemoryCache()
+        })
+
+        const MESSAGE_QUERY = gql`
+        query MyQuery {
+            getMessage
+        }
+        `;
+
+        async function getMessage() {
+            console.log("Running getMessage");
+            try {
+                const message = await client.query({ query: MESSAGE_QUERY })
+                console.log(message);
+            } catch(err) {
+                console.log(err);
+            }
+        }
+
+        getMessage();
+
+
+
+        
+
+        const INVENTORY_SUBSCRIPTION = gql`
+          subscription OnUpdateInventory {
+            inventoryUpdates
+          }
+        `;
+
+
 
         const myRequest = Axios.CancelToken.source();
         // Call serverless function to get stock from Snipcart API
@@ -32,7 +81,6 @@ const ShopPool = ({products, site}) => {
 
         return () => {
             myRequest.cancel();
-            socket.close();
         }
     }, [site])
 
---
+++ src/config/appsyncConfig.js
@@ -0,0 +1,8 @@
+const config = {
+    APPSYNC_API_KEY: 'da2-ebbehom2rfdtncy74oubefqk4u',
+    APPSYNC_GRAPHQL_API_ID: 'i2a6hmnjpffwjbvqluhgstka3u',
+    APPSYNC_REGION: 'us-east-2',
+    APPSYNC_AUTH_TYPE: 'API_KEY'
+}
+
+export default config;
